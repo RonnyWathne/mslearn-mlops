3a6d14f8eebace78fb6db86c5bfd1df74a186744
Ronny Wathne <spacew@gmail.com>
2023-12-01 21:24:31 +0100

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,4 +1,8 @@
 # Import libraries
+import mlflow
+#from xgboost import XGBClassifier
+#from sklearn.metrics import accuracy_score
+
 
 import argparse
 import glob
@@ -7,12 +11,12 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,7 +38,8 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def  split_data(df):
+    return X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
